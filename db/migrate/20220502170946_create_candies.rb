72afb42219b13e389286643534b6c1e923ec2985
Shantel <snchristian8@gmail.com>
2022-05-02 13:17:43 -0400

    adds candy table

--- db/migrate/20220502170946_create_candies.rb
+++ db/migrate/20220502170946_create_candies.rb
@@ -1,4 +1,11 @@
 class CreateCandies < ActiveRecord::Migration[6.1]
   def change
+    create_table :candies do |t|
+      t.string :name
+      t.float  :price
+      t.integer :inventory
+      t.string :image
+      t.string :quantity
+    end
   end
 end
