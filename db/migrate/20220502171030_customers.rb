25dffcc6da28f08ef5e0985a70071a66f1f571bd
Shantel <snchristian8@gmail.com>
2022-05-02 13:23:41 -0400

    adds customers table

--- db/migrate/20220502171030_customers.rb
+++ db/migrate/20220502171030_customers.rb
@@ -1,4 +1,9 @@
 class Customers < ActiveRecord::Migration[6.1]
   def change
+    create_table :customers do |t|
+      t.string :name
+      t.integer :phone_number
+      t.string :email   
+    end
   end
 end
