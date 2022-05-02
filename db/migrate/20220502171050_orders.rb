c938533441b9bbc7e8b0166ed07ed902d292e478
Shantel <snchristian8@gmail.com>
2022-05-02 13:19:01 -0400

    adds order table

--- db/migrate/20220502171050_orders.rb
+++ db/migrate/20220502171050_orders.rb
@@ -1,4 +1,13 @@
 class Orders < ActiveRecord::Migration[6.1]
   def change
+    create_table :orders do |t|
+      t.string :order_status
+      t.string :address
+      t.string :state
+      t.string :zipcode
+      t.integer :customer_id
+      t.integer :credit_card_number
+      t.string :card_holder_name
+      t.integer :cvc
   end
 end
