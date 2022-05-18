f56aae500bb85d72a51cdb3748a7968a3eec0825
Shantel <snchristian8@gmail.com>
2022-05-18 02:11:48 -0400

    added customer model

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,9 @@
 class Customer < ActiveRecord::Base
     has_many  :orders
-    
+
+    def orderd_candies
+        self.orders.map do |order|
+            order.full_order
+        end  
+    end 
 end
